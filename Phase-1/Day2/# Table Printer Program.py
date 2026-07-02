61da3c25245862434259febf13e51e72a7375268
Kabeer-7080 <mohamedkabeerwork@gmail.com>
2026-07-02 10:17:58 +0300

    Delete Phase-1/Day2 directory

--- Phase-1/Day2/# Table Printer Program.py
+++
@@ -1,9 +0,0 @@
-# Table Printer Program
-
-number = int(input("Enter a number: "))
-
-print("\nMultiplication Table")
-
-for i in range(1, 11):
-    result = number * i
-    print(f"{number} x {i} = {result}")
